ab1625ef49149d4f2a769de8d3f43a0c2c406788
apolloproductionsdj <apolloproductionsdj@gmail.com>
2020-07-29 14:13:58 -0700

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCYoQKlx3nsYQJk9czFLk1CVoxErwuX8tE",
+  authDomain: "clone-7303e.firebaseapp.com",
+  databaseURL: "https://clone-7303e.firebaseio.com",
+  projectId: "clone-7303e",
+  storageBucket: "clone-7303e.appspot.com",
+  messagingSenderId: "582400531517",
+  appId: "1:582400531517:web:508db920064cd772b45e54",
+  measurementId: "G-0BY26PZ3BS"
+};
