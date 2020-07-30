32da3583c3ecd41fbbb14fd745b11639798e4d1e
apolloproductionsdj <apolloproductionsdj@gmail.com>
2020-07-30 02:47:11 -0700

    just started <Search /> by adding Filter icon and styling the filter zrea

--- src/App.js
+++ src/App.js
@@ -3,22 +3,31 @@ import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import RecommendedVideos from './RecommendedVideos';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import SearchPage from './SearchPage';
 
 
 function App() {
   return (
     <div className="app">
-      <Header />
+      <Router>
+        <Header />
 
-      {/* Header */}
-    <div className="app_page">
-      
-
-      <Sidebar />
-      {/* RecommendedVideos */}
-
-      <RecommendedVideos />
-    </div>
+        <Switch>
+          <Route path="/search/:searchTerm">
+            <div className="app__page">
+              <Sidebar />
+              <SearchPage />
+            </div>
+          </Route>
+          <Route path="/">
+            <div className="app__page">
+              <Sidebar />
+              <RecommendedVideos />
+            </div>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import VideoCallSharpIcon from '@material-ui/icons/VideoCallSharp';
@@ -6,22 +6,35 @@ import NotificationsIcon from '@material-ui/icons/Notifications';
 import AppsIcon from '@material-ui/icons/Apps';
 import Avatar from '@material-ui/core/Avatar';
 import './Header.css'; 
+import { Link } from 'react-router-dom';
+
 
 function Header() {
+  const [inputSearch, setInputSearch] = useState('');
+
   return (
     <div className="header">
       <div className="header_left">
         <MenuIcon />
-        <img 
-          className="header_logo"
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/1024px-YouTube_Logo_2017.svg.png" 
-          alt="" 
-        />
+        <Link to="/">
+          <img 
+            className="header_logo"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/YouTube_Logo_2017.svg/1024px-YouTube_Logo_2017.svg.png" 
+            alt="" 
+          />
+        </Link>
       </div>
 
       <div className="header_input">
-        <input placeholder="Search" type="text" />
+        <input 
+          onChange={e => setInputSearch(e.target.value)} 
+          value={inputSearch} 
+          placeholder="Search" 
+          type="text" 
+        />
+        <Link to={`/search/${inputSearch}`}>
         <SearchIcon className="header_inputButton" />
+        </Link>
       </div>
 
       <div className="header_icons">
