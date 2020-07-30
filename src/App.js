35a15c5a2e3ad4f1d1f8d0dd1fd452d7c964db81
apolloproductionsdj <apolloproductionsdj@gmail.com>
2020-07-29 23:27:14 -0700

    finished with sidebar

--- src/App.js
+++ src/App.js
@@ -1,38 +1,24 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import Sidebar from './Sidebar';
+import RecommendedVideos from './RecommendedVideos';
+
 
 function App() {
   return (
     <div className="app">
-
-      {/* Header */}
-
       <Header />
 
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      {/* Sidebar */}
+      {/* Header */}
+    <div className="app_page">
+      
+
+      <Sidebar />
       {/* RecommendedVideos */}
 
+      <RecommendedVideos />
+    </div>
     </div>
   );
 }
