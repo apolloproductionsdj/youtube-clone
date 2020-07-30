83ad3fd3f63e4c24820791d07253b0be42c8ef83
apolloproductionsdj <apolloproductionsdj@gmail.com>
2020-07-30 11:24:48 -0700

    finished <ChannelRow />

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './SearchPage.css';
 import TuneOutlinedIcon from '@material-ui/icons/TuneOutlined';
+import ChannelRow from './ChannelRow';
+import './ChannelRow.css';
 
 
 function SearchPage() {
@@ -10,6 +12,16 @@ function SearchPage() {
         <TuneOutlinedIcon />
         <h2>FILTER</h2>
       </div>
+      <hr />
+
+      <ChannelRow 
+        image="https://yt3.ggpht.com/a/AATXAJydjgDHqK3vz4iHkrPZjHSsjwh1aPsqOwxHCZ_ISA=s176-c-k-c0x00ffffff-no-rj-mo"
+        channel="Clever Programmer" 
+        verified
+        subs="695K"
+        noOfVideos={382} 
+        description="You can find awesome programming lessons here! Also, expect programming tips and tricks that will take your codings skills to the..."
+      />
       <hr />
     </div>
   );
